07bcc3fce443f20a9813799a6c7d347993ef4d7e
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-12-22 23:23:38 -0800

    Add dependecies

--- Package.swift
+++ Package.swift
@@ -8,13 +8,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Hondana",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "Files",
+            ]),
         .testTarget(
             name: "HondanaTests",
             dependencies: ["Hondana"]),
