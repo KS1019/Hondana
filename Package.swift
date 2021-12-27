8bb25fe315d3870c4884625ddaf89bd910257f1b
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-12-26 19:08:09 -0800

    Add new dependencies

--- Package.swift
+++ Package.swift
@@ -13,6 +13,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
+        .package(url: "https://github.com/onevcat/Rainbow", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/scottrhoyt/SwiftyTextTable", .upToNextMajor(from: "0.9.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,6 +25,8 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 "Files",
                 "Models",
+                "Rainbow",
+                "SwiftyTextTable",
             ]),
         .target(name: "Models"),
         .testTarget(
