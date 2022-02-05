7fc3039ea74dd7f050e4f65d548d4313c8d07abd
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-02-05 01:05:45 -0800

    Move version on its own file

--- Sources/Commands/Hondana.swift
+++ Sources/Commands/Hondana.swift
@@ -4,7 +4,7 @@ import ArgumentParser
 struct Hondana: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: Constants.commandName, abstract: Constants.abstract,
-        discussion: Constants.discussion, version: Constants.version,
+        discussion: Constants.discussion, version: version,
         subcommands: [
             Sync.self, List.self, Init.self
         ])
@@ -15,6 +15,5 @@ extension Hondana {
         static let commandName = "hondana"
         static let abstract = "`hondana` helps you manage bookmarklets."
         static let discussion = "`hondana` is the root command to access other subcommands in order to manage your bookmarklets."
-        static let version = "0.0.3"
     }
 }
