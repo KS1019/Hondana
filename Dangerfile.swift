4f636e6b442da8795e97fdf9f003fde1ce079e8f
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-02-06 17:45:00 -0800

    Add comments to `Dangerfile.swift`

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,6 +4,8 @@ let danger = Danger()
 let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
 if allSourceFiles.first(where: { $0.fileType == .swift }) != nil {
+    // Only modified and created files are linted.
+    // See https://github.com/danger/swift/blob/d951a755f7e9335321160272698a2ede9dc572a3/Sources/Danger/Plugins/SwiftLint/SwiftLint.swift#L91
     let violation = SwiftLint.lint()
     if violation.isEmpty {
         message("No violation found!!!")
