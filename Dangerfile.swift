29b001c7db804cb33c933f23a1c2648c7e8b1f75
Kotaro Suto <ng1a5ss43@gmail.com>
2022-02-06 17:56:36 -0800

    Fix danger (#14)
    * Fix danger.yml
    
    * Remove unnecessary lines
    
    * Fix permission
    
    * Update danger.yml
    
    * Fix danger.yml
    
    * Revert "Fix danger.yml"
    
    This reverts commit 5c51bbea7f52c28caf6b3f13b9742530d43ba6ab.
    
    * Testing Swiftlint on PR / Revert this later
    
    * Revert "Testing Swiftlint on PR / Revert this later"
    
    This reverts commit 7437970dd8fed9d4e5af003a99440ce8ff2295e8.
    
    * Add comments to `Dangerfile.swift`
    
    * Disable line_length in Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,13 +1,14 @@
+// swiftlint:disable line_length
 import Danger
 
 let danger = Danger()
 let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
 if allSourceFiles.first(where: { $0.fileType == .swift }) != nil {
+    // Only modified and created files are linted.
+    // See https://github.com/danger/swift/blob/d951a755f7e9335321160272698a2ede9dc572a3/Sources/Danger/Plugins/SwiftLint/SwiftLint.swift#L91
     let violation = SwiftLint.lint()
     if violation.isEmpty {
         message("No violation found!!!")
     }
-} else {
-    message("No .swift file was added")
 }
