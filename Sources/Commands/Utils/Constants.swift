688be2c02a76519f6a9d9c60b5a9b8ef7058552f
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-03-03 23:46:45 -0800

    Run `swiftlint --fix`

--- Sources/Commands/Utils/Constants.swift
+++ Sources/Commands/Utils/Constants.swift
@@ -7,7 +7,7 @@ enum Constants {
     static let bookmarkletsDir = "Bookmarklets/"
 
     // swiftlint:disable force_try
-    static var rootFolder : Folder {
+    static var rootFolder: Folder {
         if ProcessInfo.processInfo.environment["CI"] == nil
             && ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil {
             return Folder.temporary
@@ -15,7 +15,7 @@ enum Constants {
             return try! Folder(path: rootDir)
         }
     }
-    
+
     static let hondanaFolder: Folder = try! rootFolder.subfolder(at: hondanaDir)
     static let bookmarkletsFolder: Folder = try! hondanaFolder.subfolder(at: bookmarkletsDir)
 }
--- Tests/HondanaTests/HondanaTests.swift
+++ Tests/HondanaTests/HondanaTests.swift
@@ -83,14 +83,14 @@ final class HondanaTests: XCTestCase {
         return Bundle.main.bundleURL
       #endif
     }
-    
+
     enum Constants {
         static let rootDir = "~/"
         static let hondanaDir = ".Hondana/"
         static let bookmarkletsDir = "Bookmarklets/"
-        
+
         // swiftlint:disable force_try
-        static var rootFolder : Folder {
+        static var rootFolder: Folder {
             if ProcessInfo.processInfo.environment["CI"] == nil
                 && ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil {
                 return Folder.temporary
@@ -98,7 +98,7 @@ final class HondanaTests: XCTestCase {
                 return try! Folder(path: rootDir)
             }
         }
-        
+
         static let hondanaFolder: Folder = try! rootFolder.subfolder(at: hondanaDir)
         static let bookmarkletsFolder: Folder = try! hondanaFolder.subfolder(at: bookmarkletsDir)
     }
