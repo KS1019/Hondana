93da8eade4202055b7a7c4e596bcaca4b5145233
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-03-05 17:32:49 -0800

    Add UtilsTests.swift

---
+++ Tests/HondanaTests/UtitlsTests.swift
@@ -0,0 +1,42 @@
+import Foundation
+import Extensions
+import XCTest
+import Files
+import AssertSwiftCLI
+
+final class UtilsTests: XCTestCase {
+    enum Utils {
+        static func generateHTML(from jsFiles: [File]) throws -> File {
+            let rawHTMLstring = """
+            <!doctype html>
+            <html>
+            <title>Bookmarklets</title>
+            <h1>Bookmarklets</h1>
+            """ +
+            (try jsFiles
+                .map {
+                    aTag(url: try $0.readAsString(encodedAs: .utf8).withJSPrefix.minified,
+                         title: $0.nameExcludingExtension.components(separatedBy: "+")[1])
+                }
+                .joined(separator: "\n")
+            )
+            + """
+            </html>
+            """
+            return try Constants.hondanaFolder
+                .createFile(at: "bookmarklets.html", contents: rawHTMLstring.data(using: .utf8))
+        }
+
+        private static func aTag(url: String, title: String) -> String {
+            return "<a href=\"\(url)\">\(title)</a>"
+        }
+    }
+    
+    func testGenerateHTML() throws {
+        let bookmarkJS = try File(path: #file).parent!.parent!.file(at: "Fixtures/+test.js")
+        let bookmarkHtml = try File(path: #file).parent!.parent!.file(at: "Fixtures/bookmarklets.html")
+
+        let file = try Utils.generateHTML(from: [bookmarkJS])
+        XCTAssertEqual(try file.readAsString().trimmingLines(), String(try bookmarkHtml.readAsString().trimmingLines().dropLast()))
+    }
+}
