c03bf7c29c1154af1cd2bb3e1c2fcfbcfab04157
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-04-19 00:02:14 -0700

    Remove one condition

--- Sources/Extensions/Files+Extensions.swift
+++ Sources/Extensions/Files+Extensions.swift
@@ -2,6 +2,6 @@ import struct Files.File
 
 extension File {
     public var isBookmarklet: Bool {
-        self.extension! == "js" && ((try? self.readAsString()) ?? "").hasSuffix("();\n") && self.nameExcludingExtension.contains("+")
+        self.extension! == "js" && self.nameExcludingExtension.contains("+")
     }
 }
