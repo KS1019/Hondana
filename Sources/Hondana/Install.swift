a464ccebb7bac9fbe86f7a683eb07b50ccb541a1
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-05-27 22:56:17 -0700

    Modify Install command

--- Sources/Hondana/Install.swift
+++ Sources/Hondana/Install.swift
@@ -7,7 +7,7 @@ struct Install: ParsableCommand {
                                                     abstract: Constants.Install.abstract,
                                                     discussion: Constants.Install.discussion)
     
-    @Argument(help: "")
+    @Argument(help: "Experimental - `hondana install` installs an bookmarklet from remote repository on GitHub.")
     var bookmarklet: String
 }
 
@@ -15,14 +15,25 @@ extension Install {
     func run() throws {
         let comp = bookmarklet.components(separatedBy: "/")
         if comp.count == 3 {
-            let path = Folder.temporary.path
-            try Git.clone(repo: comp[0]+"/"+comp[1] , path: path)
-            let folder = try Folder(path: path)
-            let file = folder.files.filter { $0.nameExcludingExtension.contains(comp[2]) }.first!
-            let bookmarkletFile = Bookmarklet(file: file)
-            try Utils.write(bookmarklets: [bookmarkletFile!], to: .hondanaDir)
+            let path: String
+            if !Folder.temporary.containsSubfolder(at: "hondana-git-files/\(comp[0])/\(comp[1])") {
+                path = try Folder.temporary.createSubfolder(at: "hondana-git-files/\(comp[0])/\(comp[1])").path
+                try Git.clone(repo: comp[0]+"/"+comp[1] , path: path) {
+                    let folder = try Folder(path: path)
+                    let file = folder.files.filter { $0.nameExcludingExtension.contains(comp[2]) }.first!
+                    let bookmarkletFile = Bookmarklet(file: file)!
+                    try Utils.write(bookmarklets: [bookmarkletFile], to: .hondanaDir)
+                }
+            } else {
+                path = try Folder.temporary.createSubfolderIfNeeded(at: "hondana-git-files/\(comp[0])/\(comp[1])").path
+                try Git.pull(repo: comp[0]+"/"+comp[1] , path: path) {
+                    let folder = try Folder(path: path)
+                    let file = folder.files.filter { $0.nameExcludingExtension.contains(comp[2]) }.first!
+                    let bookmarkletFile = Bookmarklet(file: file)!
+                    try Utils.write(bookmarklets: [bookmarkletFile], to: .hondanaDir)
+                }
+            }
         }
-        
     }
 }
 
