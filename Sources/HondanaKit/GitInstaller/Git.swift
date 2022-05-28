4614e95fb7a1531d7c6f96355d443f290767ade2
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-05-27 22:55:07 -0700

    Add pull function

--- Sources/HondanaKit/GitInstaller/Git.swift
+++ Sources/HondanaKit/GitInstaller/Git.swift
@@ -1,14 +1,32 @@
-@_implementationOnly import class Foundation.Process
-@_implementationOnly import struct Foundation.URL
+import Foundation
 import Files
 
 public class Git {
     private static let url = URL(fileURLWithPath: "/usr/bin/git")
     public let exists: Bool = (try? File(path: "/usr/bin/git")) != nil
-    private static let process =  Process()
-    public static func clone(repo: String, path: String) throws {
+    private static let process = Process()
+    public static func clone(repo: String, path: String, closure: @escaping () throws -> Void) throws {
         process.executableURL = url
-        process.arguments = ["clone", "https://github.com/" + repo + ".git", path]
+        process.arguments = ["clone", "https://github.com/" + repo + ".git", path, "-q"]
+        process.terminationHandler = { p in
+            if p.terminationStatus == 0 {
+                try? closure()
+            }
+        }
         try process.run()
+        process.waitUntilExit()
+    }
+    
+    public static func pull(repo: String, path: String, closure: @escaping () throws -> Void) throws {
+        FileManager.default.changeCurrentDirectoryPath(path)
+        process.executableURL = url
+        process.arguments = ["pull"]
+        process.terminationHandler = { p in
+            if p.terminationStatus == 0 {
+                try? closure()
+            }
+        }
+        try process.run()
+        process.waitUntilExit()
     }
 }
