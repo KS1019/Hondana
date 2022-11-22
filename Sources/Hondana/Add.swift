d1244f0d87c4f0f49e401009b84c2433fd4f51a2
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-11-22 00:29:42 -0800

    Fix error handling

--- Sources/Hondana/Add.swift
+++ Sources/Hondana/Add.swift
@@ -18,28 +18,16 @@ struct Add: ParsableCommand {
 extension Add {
     func validate() throws {
         guard !bookmarkletName.isEmpty else { throw ValidationError("Bookmarklet name needs to be specified") }
-        do {
-            if try FileSystem.home
-                .subfolder(at: FileSystem.hondanaDir)
-                .subfolder(at: FileSystem.bookmarkletsDir)
-                .containsFile(named: fileName)
-            {
-                throw ValidationError("\(fileName) already exists. Change Bookmarklet Name")
-            }
-        } catch {
-            fatalError("Could not reach \(FileSystem.bookmarkletsDir)")
+        if try FileSystem.home.subfolder(at: FileSystem.hondanaDir).subfolder(at: FileSystem.bookmarkletsDir).containsFile(named: fileName) {
+            throw ValidationError("\(fileName) already exists. Change Bookmarklet Name")
         }
     }
 
     func run() throws {
-        do {
-            try FileSystem.home
-                .subfolder(at: FileSystem.hondanaDir)
-                .subfolder(at: FileSystem.bookmarkletsDir)
-                .createFile(named: fileName)
-        } catch {
-            fatalError("Failed to create \(fileName)")
-        }
+        try FileSystem.home
+            .subfolder(at: FileSystem.hondanaDir)
+            .subfolder(at: FileSystem.bookmarkletsDir)
+            .createFile(named: fileName)
     }
 }
 
